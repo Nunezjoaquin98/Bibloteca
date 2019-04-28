579c465fc73485d8a65856730385d5bafbcd167e
Nunezjoaquin98 <40808656+Nunezjoaquin98@users.noreply.github.com>
2019-04-28 16:50:01 -0300

    Add files via upload

---
+++ bibloteca/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Funciones.h"
+#include "ArraysEmployees.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
