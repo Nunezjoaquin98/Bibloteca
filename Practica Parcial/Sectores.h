4be89d1fe6f2d967f950c9c3e7fd3a59056f7e15
Nunezjoaquin98 <40808656+Nunezjoaquin98@users.noreply.github.com>
2019-04-29 21:15:36 -0300

    Add files via upload

---
+++ Practica Parcial/Sectores.h
@@ -0,0 +1,13 @@
+typedef struct
+{
+    int id;
+    char descripcion[51];
+}eSector;
+
+
+/** \brief Muestra todos los sectores cargados con su id y descripcion
+ *
+ *
+ *
+ */
+void mostrarSectores();
---
+++ Practica Parcial/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+#include "ArraysEmployees.h"
+
+
+int main()
+{
+    char salir = 'n';
+    eEmployee empleados[SIZE]; // se crea un arrays tipo eEmployee con el tamaño de 1000
+
+    initEmployees(empleados,SIZE); // Se inicializa todos los empleados en 1.
+    hardcodeoEmpleados(empleados);
+    do
+    {
+        switch(menu())
+        {
+        case 1:
+            addEmployee(empleados,SIZE);
+            break;
+        case 2:
+            if(hayEmpleadoCargado(empleados,SIZE))
+            {
+                modifyEmployee(empleados,SIZE);
+                break;
+            }
+            else
+            {
+                printf("***Error*** No hay ningun empleado cargado.\n");
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 3:
+            if(hayEmpleadoCargado(empleados,SIZE))
+            {
+
+                removeEmployee(empleados,SIZE,0);
+                break;
+            }
+            else
+            {
+                printf("***Error*** No hay ningun empleado cargado.\n");
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 4:
+            if(hayEmpleadoCargado(empleados,SIZE))
+            {
+                sortEmployees(empleados,SIZE);
+                showEmployees(empleados,SIZE);
+                function_averageSalary(empleados,SIZE);
+                system("pause");
+            }
+            else
+            {
+                printf("***Error*** No hay ningun empleado cargado.\n");
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 6:
+            mostrarNacionalidades();
+        case 5:
+            printf("\n\t***Saliendo***\n\n");
+            salir = 's';
+            system("pause");
+            system("cls");
+            break;
+        default:
+            printf("*** ERROR *** Debe ingresar un numero del 1 al 5\n\n") ; // el default se utiliza por si se ingresa un numero que no sea del 1 al 5
+            system("pause");
+            system("cls");
+            break;
+        }
+    }
+    while(salir == 'n');
+
+
+
+
+    return 0;
+}
